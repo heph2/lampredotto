7ab55b524735d9f319925fc1af8a641660f8ed31
heph <srht@mrkeebs.eu>
2024-02-16 18:29:55 +0100

    feat: add hello world draft controller

---
+++ src/HelloWorld.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace Lampredotto;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class HelloWorld
+{
+    protected $logger;
+    protected $view;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->logger = $container->get('logger');
+        $this->view = $container->get('view');
+    }
+
+    public function __invoke(): void
+    {
+        $this->logger->info("Home page action dispatched");
+        echo $this->view->render('hello.twig', ['name' => 'Fabien']);
+        // echo 'Hello, World!';
+    }
+    // public function __invoke(Request $request, Response $response, string $template, array $params = []): Response
+    // {
+    //     // return $this->view->render($response, $template, $params);
+    //     echo $this->view->render('hello.twig', $params);
+    // }
+}
