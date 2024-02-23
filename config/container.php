4cb45e88a42acf72aa8b9ea3920d3ee4e4d3b6f6
heph <srht@mrkeebs.eu>
2024-02-23 22:41:50 +0100

    feat: configure DB and Requests/response according to PSR-7

--- config/container.php
+++ config/container.php
@@ -6,6 +6,10 @@ use Monolog\Logger;
 use Monolog\Processor\UidProcessor;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
+use Psr\Http\Message\ServerRequestInterface;
+use Laminas\Diactoros\ServerRequestFactory;
+use Psr\Http\Message\ResponseInterface;
+use Nyholm\Psr7\Response;
 
 return [
     'settings' => function () {
@@ -31,8 +35,25 @@ return [
         $loader = new FilesystemLoader(__DIR__ . '/../templates');
         return new Environment($loader);
     },
-    // 'view' => function (ContainerInterface $container) {
-    //     $settings = $container->get('settings');
-    //     return Twig::create($settings['view']['template_path'], $settings['view']['twig']);
-    // },
+    
+    'pdo' => function (ContainerInterface $container) {
+        $settings = $container->get('settings');
+
+        $host = $settings['db']['host'];
+        $dbname = $settings['db']['database'];
+        $username = $settings['db']['username'];
+        $password = $settings['db']['password'];
+        $charset = $settings['db']['charset'];
+        $flags = $settings['db']['flags'];
+        $dsn = "pgsql:host=$host;port=5432;dbname=$dbname";
+        return new PDO($dsn, $username, $password);
+    },
+    ServerRequestInterface::class => function () {
+        return ServerRequestFactory::fromGlobals(
+            $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
+        );
+    },
+    ResponseInterface::class => function () {
+        return new Response();
+    },
 ];
--- config/settings.php
+++ config/settings.php
@@ -30,6 +30,25 @@ $settings = [
             'debug' => true,
             'auto_reload' => true,
         ],
+    ],
+    "db" => [
+        'driver' => 'pgsql',
+        'host' => 'localhost',
+        'username' => 'postgres',
+        'database' => 'lampredotto_staging',
+        'password' => 'postgres',
+        'charset' => 'utf8mb4',
+        'collation' => 'utf8mb4_unicode_ci',
+        'flags' => [
+            // Turn off persistent connections
+            PDO::ATTR_PERSISTENT => false,
+            // Enable exceptions
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            // Emulate prepared statements
+            PDO::ATTR_EMULATE_PREPARES => true,
+            // Set default fetch mode to array
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ],
     ],    
 ];
 return $settings;
